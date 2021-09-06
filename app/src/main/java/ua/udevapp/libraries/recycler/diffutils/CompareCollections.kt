6201276f6548f22de7568657d987c02e77c41f66
Stanislav Humeniuk <stanislav.humeniuk@gmail.com>
2021-09-06 12:54:19 +0300

    Create item diff utils callback

---
+++ app/src/main/java/ua/udevapp/libraries/recycler/diffutils/CompareCollections.kt
@@ -0,0 +1,14 @@
+package ua.udevapp.libraries.recycler.diffutils
+
+interface CompareCollections<in D> {
+    fun updateCollections(old: Collection<D>?, new: Collection<D>?) {
+        old?.let { updateOldCollection(it) }
+        new?.let { updateNewCollection(it) }
+    }
+
+    fun updateOldCollection(collection: Collection<D>)
+    fun updateNewCollection(collection: Collection<D>)
+
+    fun areItemsTheSame(old: D?, new: D?) : Boolean
+    fun areContentsTheSame(old: D?, new: D?) : Boolean
+}
