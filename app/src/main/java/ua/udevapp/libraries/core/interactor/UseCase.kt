40f90680ab546dcf447dfdcf7d1425a0e4e646fb
Stanislav Humeniuk <stanislav.humeniuk@gmail.com>
2021-09-07 18:45:38 +0300

    Create core package with basic common classes. Add Hilt as DI, Lifecycle, and Timber as Logger

---
+++ app/src/main/java/ua/udevapp/libraries/core/interactor/UseCase.kt
@@ -0,0 +1,26 @@
+package ua.udevapp.libraries.core.interactor
+
+import kotlinx.coroutines.*
+import ua.udevapp.libraries.core.exceptions.Failure
+import ua.udevapp.libraries.core.functional.Either
+
+/**
+ * Abstract class for a Use Case (Interactor in terms of Clean Architecture).
+ * This abstraction represents an execution unit for different use cases (this means than any use
+ * case in the application should implement this contract).
+ *
+ * By convention each [UseCase] implementation will execute its job in a background thread
+ * (kotlin coroutine) and will post the result in the UI thread.
+ */
+abstract class UseCase<out Type, in Params> where Type : Any {
+
+    abstract suspend fun run(params: Params): Either<Failure, Type>
+
+    @DelicateCoroutinesApi
+    operator fun invoke(params: Params, onResult: (Either<Failure, Type>) -> Unit = {}) {
+        val job = GlobalScope.async(Dispatchers.IO) { run(params) }
+        GlobalScope.launch(Dispatchers.Main) { onResult(job.await()) }
+    }
+
+    class None
+}
