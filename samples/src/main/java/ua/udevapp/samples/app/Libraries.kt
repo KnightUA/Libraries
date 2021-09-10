e4d39651731ee6928b661fd6f64af3e70a7b545a
Stanislav Humeniuk <stanislav.humeniuk@gmail.com>
2021-09-10 20:48:41 +0300

    Add strict mode for debug version, rename strings resources

--- samples/src/main/java/ua/udevapp/samples/app/Libraries.kt
+++ samples/src/main/java/ua/udevapp/samples/app/Libraries.kt
@@ -4,6 +4,10 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
 import ua.udevapp.libraries.BuildConfig
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
+
 
 @HiltAndroidApp
 class Libraries : Application() {
@@ -11,11 +15,31 @@ class Libraries : Application() {
         super.onCreate()
 
         this.initializeLogger()
+        this.enableStrictModeWhen(BuildConfig.DEBUG)
     }
 
     private fun initializeLogger() {
         if (BuildConfig.DEBUG)
             Timber.plant(Timber.DebugTree())
+    }
+
+    private fun enableStrictModeWhen(predicate: Boolean) {
+        if(predicate) {
+            StrictMode.setThreadPolicy(
+                ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build()
+            )
+        }
     }
 
 }
